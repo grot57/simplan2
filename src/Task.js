0990a8530fc1b63e0564e154a0cfe246ab06ee5d
grot57 <40370630+grot57@users.noreply.github.com>
2018-08-06 01:12:58 +0300

    add right task handle for resize

--- src/Task.js
+++ src/Task.js
@@ -58,6 +58,41 @@ class TaskRightHandle extends Component {
     }
 }
 
+class TaskLeftHandle extends Component {
+    render() {
+        let bluePattern = 'repeating-linear-gradient(#606dbc,#606dbc 2px,#465298 2px,#465298 4px'
+        let yellowPpattern = 'repeating-linear-gradient(to right,#f6ba52,#f6ba52 2px,#ffd180 2px,#ffd180 4px';
+        let {isOver} = this.props
+
+        let style = {
+            transition: "left 0.2s ease-in-out, top 0.5s ease-in-out, background 0.5s ease-in-out",
+            marginLeft: "0px",
+            background: isOver ? yellowPpattern : bluePattern,
+            width: "10px",
+            zIndex: 10,
+            textAlign: "right",
+            float: "right",
+            verticalAlign: "middle",
+            justifyContent: 'center',
+            lineHeight: LANE_HEIGHT-4 + "px",
+            position: "absolute",
+            right: "0px",
+            top:"0px",
+            height: LANE_HEIGHT-4 + "px" ,
+            cursor: "ew-resize",
+            borderRadius: "4px",
+            opacity: "0.7",
+
+        };
+
+        return (
+            <span title="change length" style={style}>
+
+            </span>
+        )
+    }
+}
+
 class Task extends Component {
     state = {
         name: "undefined",
@@ -135,8 +170,10 @@ class Task extends Component {
                     <TaskRightHandle />
                     <span  style={{marginLeft: "10px"}}>
                         {task.done === "true" ? <Glyphicon style={{color:"LawnGreen"}} title="Done!" glyph="ok-sign" /> : ""}
-                        {task.priority === "high" ? <Glyphicon style={{color:"GoldenRod"}} title="High priority" glyph="arrow-up" /> : ""} {name}
-                        </span>
+                        {task.priority === "high" ? <Glyphicon style={{color:"GoldenRod"}} title="High priority" glyph="arrow-up" /> : ""}
+                        <span> {name}</span>
+                    </span>
+                    <TaskLeftHandle />
                 </div>
         );
     }
